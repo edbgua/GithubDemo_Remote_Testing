72d1b0b68f0273c809dbd327e0432642be451072
Gudleif <edbgua@mac.com>
2023-12-02 22:11:28 +0100

    Endret tekst

--- GithubDemo/ContentView.swift
+++ GithubDemo/ContentView.swift
@@ -6,14 +6,14 @@
 //
 
 import SwiftUI
-
+ // Body
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dette er en test")
         }
         .padding()
     }
