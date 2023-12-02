6c496c701c7752baf6e71c4cd4c32d4db1ea8b34
Gudleif <edbgua@mac.com>
2023-12-02 22:52:05 +0100

    Lagt til secondView

---
+++ GithubDemo/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  GithubDemo
+//
+//  Created by Gudleif Aronsen on 02/12/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, Gudleif!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
